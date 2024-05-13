dfa48c9751671769d8ad79b3305bddc13d63d7a0
aleksandr_katsur <alexander_katzur@mail.ru>
2024-05-13 18:14:30 +0700

    Сменил возвращаемый тип в контроллере

--- src/main/java/su/soviet/loanMVC/controller/LoanController.java
+++ src/main/java/su/soviet/loanMVC/controller/LoanController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import su.soviet.loanMVC.service.LoanService;
 
 @Controller
@@ -18,9 +19,9 @@ public class LoanController {
     }
 
     @GetMapping("/loan")
-    public String getLoanByUserId(@RequestParam(value = "userId") Long userId,
+    @ResponseBody
+    public Integer getLoanByUserId(@RequestParam(value = "userId") Long userId,
                                   Model model) {
-        model.addAttribute("loan", service.assessLoan(userId));
-        return "loan";
+        return service.assessLoan(userId);
     }
 }
